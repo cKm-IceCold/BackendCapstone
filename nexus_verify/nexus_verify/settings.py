8054d98a2630306a0717ec31938c4b2622116a64
Paul Emmanuel Chukwuma <chukspaul777@gmail.com>
2025-12-26 19:57:17 +0100

    Fix: Add Render domain to ALLOWED_HOSTS

--- nexus_verify/nexus_verify/settings.py
+++ nexus_verify/nexus_verify/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-m2s2h^v=*7ri^^lhs#wq66kx-2^))(8e57u!juoyvb)+u!x*b!
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['backendcapstone-mhh2.onrender.com', 'localhost', '127.0.0.1']
 
 
 # Application definition
