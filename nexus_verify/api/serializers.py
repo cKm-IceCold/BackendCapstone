6d437a94f0349373404bd492710001d8508b4dc4
Paul Emmanuel Chukwuma <chukspaul777@gmail.com>
2025-12-26 20:11:34 +0100

    Enhance: Support token generation in UserSerializer

--- nexus_verify/api/serializers.py
+++ nexus_verify/api/serializers.py
@@ -1,11 +1,27 @@
 from rest_framework import serializers
 from .models import User, Property, AuditTransaction
 
+from rest_framework.authtoken.models import Token
+
 class UserSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(write_only=True)
+    token = serializers.SerializerMethodField()
+
     class Meta:
         model = User
-        fields = ['id', 'username', 'email', 'role', 'date_joined']
-        read_only_fields = ['id', 'date_joined']
+        fields = ['id', 'username', 'email', 'role', 'password', 'token', 'date_joined']
+        read_only_fields = ['id', 'date_joined', 'token']
+
+    def get_token(self, obj):
+        token, created = Token.objects.get_or_create(user=obj)
+        return token.key
+
+    def create(self, validated_data):
+        password = validated_data.pop('password')
+        user = User(**validated_data)
+        user.set_password(password)
+        user.save()
+        return user
 
 class PropertySerializer(serializers.ModelSerializer):
     class Meta:
