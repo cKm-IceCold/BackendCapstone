1e3bc3a70e8818c2db3f65173b9b96afa724c9ab
Paul Emmanuel Chukwuma <chukspaul777@gmail.com>
2025-12-27 20:47:31 +0100

    Fix: Enable email authentication in backend

--- nexus_verify/nexus_verify/settings.py
+++ nexus_verify/nexus_verify/settings.py
@@ -143,3 +143,9 @@ MEDIA_ROOT = BASE_DIR / 'media'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 CORS_ALLOW_ALL_ORIGINS = True
+
+AUTHENTICATION_BACKENDS = [
+    'api.backends.EmailBackend',
+    'django.contrib.auth.backends.ModelBackend',
+]
+
